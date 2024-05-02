1d4961d9a11b121986e7c81d6f6ee3dcb69967aa
akshya <akshya.nanda03@gmail.com>
2024-05-02 18:13:11 +0530

    ChannelCard done

--- src/components/ChannelCard.jsx
+++ src/components/ChannelCard.jsx
@@ -1,24 +1,54 @@
-import { Box, Card } from '@mui/material'
-import React from 'react'
-import { Link } from 'react-router-dom'
+import { Box, Card, CardContent, CardMedia } from "@mui/material";
+import React from "react";
+import { Link } from "react-router-dom";
+import { demoChannelTitle, demoProfilePicture } from "../utils/constants";
+import { CheckCircle } from "@mui/icons-material";
 
-const ChannelCard = ({channelDetail}) => {
-  return (
-    <Card sx={{
-      width: {
-          md: '350px',
-          sm: '100%'
-      },
-      // boxShadow: 'none',
-      borderRadius: '20px'
-  }}>
-    <Link to={`/channel/${}`}/>
+const ChannelCard = ({
+  channelDetail: {
+    id: { channelId },
+    snippet,
+    statistics
+  },
+}) => (
+  <Card sx={{
+    width: {
+        md: '350px',
+        sm: '100%'
+    },
+    boxShadow: 'none',
+    borderRadius: '20px',
+    display: "flex",
+    flexDirection: "column",
+    justifyContent:"center",
+    // textAlign:"center"
+    alignItems: "center",
+    background: "none",
+    margin: 'auto'
+}}>
+    <Link to={`/channel/${channelId}`}>
+    <CardMedia  
+    image={snippet?.thumbnails?.high?.url} 
+    alt = {snippet?.title}
+    sx={{
+        height: 180,  
+        width: 180,
+        borderRadius: "50%"
+    }}
+    />
+    <CardContent sx={{
+        height: '60px',
+        fontWeight: 'bold', color: "#fff",
+    }}>
+        <p>{snippet?.title || demoChannelTitle}<CheckCircle sx={{
+                        fontSize: 12, color: 'gray', ml: '5px'
+                    }} /></p>
+        {statistics?.subscriberCount && (
+          <p>{parseInt(statistics.subscriberCount).toLocaleString()}Subscribers</p>
+        )}
+    </CardContent>
+    </Link>
+</Card>
+);
 
-
-  </Card>
-
-
-  )
-}
-
-export default ChannelCard
+export default ChannelCard;
