a613d20babd7e982882ee670c963af4cfb100847
akshya <akshya.nanda03@gmail.com>
2024-05-07 11:32:02 +0530

    working on ChannelDetail

--- src/components/ChannelDetail.jsx
+++ src/components/ChannelDetail.jsx
@@ -1,8 +1,26 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom';
+import { fetchFromAPI } from '../utils/fetchFromAPI';
 
 const ChannelDetail = () => {
+
+  const [channelDetail, setChannelDetail] = useState(null);
+  const [videos, setvideos] = useState([])
+
+    const {id} = useParams();
+    console.log(channelDetail);
+
+  useEffect(()=>{
+    fetchFromAPI(`channels?part=snippet&id=${id}`)
+      .then((data)=>setChannelDetail(data.items[0]));
+    fetchFromAPI(`search?channelId=${id}&part=snippet&order=date`)
+      .then((data)=>setvideos(data.items));
+
+  }, [id]);
+
+
   return (
-    <div>ChannelDetail</div>
+    <div>{id}</div>
   )
 }
 
